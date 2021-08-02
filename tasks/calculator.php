88053c615446f501989f8a9e09282d8bb747bfaa
ramani <ramani@schemaxtech.com>
2021-08-02 16:10:37 +0530

    claculator form

---
+++ tasks/calculator.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+	<title>Web App for Simple Interest Calculation</title>
+
+       
+    </head>
+
+    <body>
+        <h1>Simple Interest Calculation</h1>
+
+        Amount: <input id="p"><br/>
+        Rate: <input id="r"><br/>
+        No. of Years: <input id="n"><br/>
+
+        <button>Calculate</button>
+
+        <p id="result"></p>
+    </body>
+</html>
